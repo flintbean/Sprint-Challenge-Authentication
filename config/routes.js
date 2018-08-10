8783e5ab45d22ed4099f45b7a601180392698d13
Austin Merando <ajmerando1@yahoo.com>
2018-08-10 09:47:05 -0700

    mvp complete

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig')
+const jwt = require('jsonwebtoken');
 const { authenticate } = require('./middlewares');
 
 module.exports = server => {
@@ -8,15 +10,64 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+const secret = require('../_secrets/keys').jwtKey;
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '24h',
+    jwtid: '8728391',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+async function register(req, res) {
   // implement user registration
+  const user = req.body;
+
+  user.password = bcrypt.hashSync(user.password, 10);
+
+  db('users')
+    .insert(user)
+    .then(ids => {
+      db('users')
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json(token)
+        })
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+
+  const credentials = req.body;
+
+  db('users')
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generateToken(user);
+        res.send(token)
+      }
+      else {
+        return res.status(401).json({ error: 'Invalid Credentials' })
+      }
+    })
+    .catch(err => res.status(500).json({ error }))
 }
 
-function getJokes(req, res) {
+async function getJokes(req, res) {
   axios
     .get(
       'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
